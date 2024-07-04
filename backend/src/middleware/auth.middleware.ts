f27bcdfc75532c16ae2085431a62a3daa4b2730c
Andyzhang222 <Andy.zhang.z@outlook.com>
2024-07-04 00:56:42 -0300

    update database data in env
    update database in env

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,12 +1,11 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwkToPem from 'jwk-to-pem';
 import jwt from 'jsonwebtoken';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-let pems: { [key: string]: any }  = {}
+let pems: { [key: string]: any } = {}
 
 class AuthMiddleware {
 
@@ -17,32 +16,35 @@ class AuthMiddleware {
     this.setUp()
   }
 
-  private verifyToken(req: Request, resp: Response, next): void {
+  private verifyToken(req: Request, resp: Response, next: NextFunction): void {
     const { token } = req.body;
     console.log("Original Token:", token);
-    if (!token) return resp.status(401).end();
+    if (!token) {
+      resp.status(401).end();
+      return;
+    }
 
     let decodedJwt: any = jwt.decode(token, { complete: true });
     if (decodedJwt === null) {
-      resp.status(401).end()
-      return
+      resp.status(401).end();
+      return;
     }
     console.log("Decoded Token:", decodedJwt);
     let kid = decodedJwt.header.kid;
     let pem = pems[kid];
-    console.log(pem)
+    console.log(pem);
     if (!pem) {
-      resp.status(401).end()
-      return
+      resp.status(401).end();
+      return;
     }
     jwt.verify(token, pem, function (err: any, payload: any) {
       if (err) {
-        resp.status(401).end()
-        return
+        resp.status(401).end();
+        return;
       } else {
-        next()
+        next();
       }
-    })
+    });
   }
 
   private async setUp() {
@@ -55,21 +57,21 @@ class AuthMiddleware {
       }
       const data = await response.json();
       const { keys } = data;
-        for (let i = 0; i < keys.length; i++) {
-          const key_id = keys[i].kid;
-          const modulus = keys[i].n;
-          const exponent = keys[i].e;
-          const key_type = keys[i].kty;
-          const jwk = { kty: key_type, n: modulus, e: exponent };
-          const pem = jwkToPem(jwk);
-          pems[key_id] = pem;
-        }
-        console.log("got PEMS")
+      for (let i = 0; i < keys.length; i++) {
+        const key_id = keys[i].kid;
+        const modulus = keys[i].n;
+        const exponent = keys[i].e;
+        const key_type = keys[i].kty;
+        const jwk = { kty: key_type, n: modulus, e: exponent };
+        const pem = jwkToPem(jwk);
+        pems[key_id] = pem;
+      }
+      console.log("got PEMS");
     } catch (error) {
-      console.log(error)
+      console.log(error);
       console.log('Error! Unable to download JWKs');
     }
   }
 }
 
-export default AuthMiddleware
+export default AuthMiddleware;
