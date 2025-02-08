996e5ce24ea8046a0bdf25128fac51ddaca3a97c
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-02-07 20:24:41 -0400

    test
    test

--- dist/frontend/src/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/config.js
-const BASE_URL = 'http://localhost';
-exports.default = BASE_URL;
-//# sourceMappingURL=config.js.map
--- dist/src/routes/authRoutes.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=authRoutes.js.map
